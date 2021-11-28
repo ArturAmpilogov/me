f50af286067a3983458608ddf1c7b5e2fd0c8d3c
ArturAmpilogov <artur.ampilogov@gmail.com>
2021-11-28 21:58:44 +0300

    Updated experience and README

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Visit [https://arturampilogov.vercel.app](https://arturampilogov.vercel.app).
 
 ## Code
 
-The website is crafted with React.JS, Node.JS, Next.JS static pages generation, CSS/SCSS modules and TypeScript.
+This website is created with React.JS, Node.JS, Next.JS static page generation, CSS/SCSS modules, CSS animation and TypeScript.
 For code check ES Lint and Stylelint are enabled.
 Github CI/CD is set up to automatically build and deploy the website to Vercel.
--- data/aboutme.ts
+++ data/aboutme.ts
@@ -1,3 +1,4 @@
 export const aboutme = 
 "Over 14 years in the Information Technology area, among them 12 years of full stack programming and software architecture experience. "+
-"Passionate about design and programming, like to help people and see information technologies improving lives.";
+"Passionate about design and programming, like to help people and see information technologies improving lives. "+
+"Located in Saint-Petersburg, Russia (GMT+3), and in Europe (GMT+1).";

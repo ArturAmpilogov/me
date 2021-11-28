4a64b56b70bf2cee10f70f637de188c0a15efc4e
ArturAmpilogov <artur.ampilogov@gmail.com>
2021-11-28 05:53:05 +0300

    Reduced photo size

--- components/custom/resume/About/About.tsx
+++ components/custom/resume/About/About.tsx
@@ -1,5 +1,4 @@
 import { AboutProps } from "./About.prop";
-import Image from "next/image";
 import styles from "./About.module.scss";
 
 export const About = ({children, name, photoUrl, ...props}: AboutProps): JSX.Element => {
@@ -8,7 +7,7 @@ export const About = ({children, name, photoUrl, ...props}: AboutProps): JSX.Ele
         <h1>About me</h1>
         <h3>{name}</h3>
         <div className={styles.container}>
-            <Image src={photoUrl} alt={`Photo ${name}`} width="128" height="145"/>
+            <img src={photoUrl} alt={`Photo ${name}`} width="150"/>
             <p>{children}</p>
         </div>
     </div>);

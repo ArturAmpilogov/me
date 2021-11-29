1bd1b41695644ea9c92dde407090b1554b9ace79
ArturAmpilogov <artur.ampilogov@gmail.com>
2021-11-29 13:48:18 +0300

    Highlighted name

--- components/custom/resume/About/About.tsx
+++ components/custom/resume/About/About.tsx
@@ -1,12 +1,13 @@
 import { AboutProps } from "./About.prop";
 import styles from "./About.module.scss";
+import cn from "classnames";
 
 export const About = ({children, name, photoUrl, ...props}: AboutProps): JSX.Element => {
     return (
-    <div {...props}>
+    <div {...props} className={cn(styles.container)}>
         <h1>About me</h1>
         <div className={styles.name}>{name}</div>
-        <div className={styles.container}>
+        <div className={styles.description}>
             <img src={photoUrl} alt={`Photo ${name}`} width="150"/>
             <p>{children}</p>
         </div>

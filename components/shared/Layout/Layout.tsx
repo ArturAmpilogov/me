e5912b5e30f90ece459935f77e4ea73cf5330490
ArturAmpilogov <artur.ampilogov@gmail.com>
2023-08-07 23:46:48 +0400

    Google analytics

--- components/shared/Layout/Layout.tsx
+++ components/shared/Layout/Layout.tsx
@@ -2,12 +2,29 @@ import { ILayoutProps } from "./Layout.props";
 import styles from "./Layout.module.scss";
 import cn from "classnames";
 import { Nav } from "../Nav/Nav";
+import Script from "next/script";
 
 export const Layout = ({ children, ...props }: ILayoutProps): JSX.Element => {
   return (
-    <div {...props} className={cn(styles.container, props.className)}>
-      <Nav />
-      <div>{children}</div>
-    </div>
+    <>
+      <div {...props} className={cn(styles.container, props.className)}>
+        <Nav />
+        <div>{children}</div>
+      </div>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-X20GD52Z7Z"
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'G-X20GD52Z7Z');
+      `,
+        }}
+      />
+    </>
   );
 };
